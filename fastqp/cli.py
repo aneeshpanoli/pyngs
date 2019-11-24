3d6b213fa51bc0e198bca3645d5a76835741788f
Aneesh <aneeshpanoli@gmail.com>
2019-11-23 20:15:56 -0800

    bug -first

--- fastqp/cli.py
+++ fastqp/cli.py
@@ -25,37 +25,26 @@ class Bunch(object):
         self.__dict__.update(adict)
 
 
-def run(arguments):
+def run(file_name, quiet=False, binsize=None, nreads=2000000, count_duplicates=False,
+            leftlimit=1, rightlimit=-1, kmer=5, base_probs='0.25,0.25,0.25,0.25,0.1',
+            text='-', output='fastqp_figures', median_qual_r=30):
     """ read FASTQ or SAM and tabulate basic metrics
     arguments is a dictionary so that we can call this as a function """
-    arguments['input'] = argparse.FileType('r')(arguments['input'])
-    arguments['text'] = argparse.FileType('w')(arguments['text'])
-    args = Bunch(arguments)  # convert back to an argparse namespace
+    input = argparse.FileType('r')(file_name)
+    text = argparse.FileType('w')(text)
     time_start = time.time()
-    if args.input.name != '<stdin>':
-        bsize = os.path.getsize(args.input.name)
-
+    in_file = os.path.basename(file_name)# filename no path
+    if file_name != '<stdin>':
+        bsize = os.path.getsize(file_name)
+    name, ext = in_file.split('.')[0], '.'+in_file.split('.')[1]
+    sample_name = name
     est_counter = int()
     sample_lengths = list()
     sample_binsizes = list()
     act_nlines = int()
-    name, ext = os.path.splitext(args.input.name)
-    if (args.leftlimit > 0) and (args.rightlimit > 0):
-        if args.rightlimit < args.leftlimit:
-            sys.exit("Left limit must be less than right limit.\n")
-    if args.type:
-        ext = '.' + args.type
-    if ext not in ['.fq', '.fastq', '.sam', '.bam', '.gz'] and args.input.name != '<stdin>':
-        sys.exit("Input file must end in either .sam, .bam, .fastq, or .fastq.gz\n")
-
-    if args.name:
-        sample_name = args.name
-    else:
-        sample_name = args.input.name
-
     # estimate the number of lines in args.input if we can
     if ext in ['.fastq', '.fq']:
-        with FastqReader(open(args.input.name)) as fh:
+        with FastqReader(open(file_name)) as fh:
             for read in fh:
                 sample_lengths.append(len(read))
                 sample_binsizes.append(len(str(read)))
@@ -65,13 +54,13 @@ def run(arguments):
             mean_bentry = mean(sample_binsizes)
             mean_len = mean(sample_lengths)
             est_nlines = int(bsize / mean_bentry)
-            if not args.quiet:
+            if not quiet:
                 sys.stderr.write("At {bytes:.0f} bytes per read of {len:.0f} length "
                                  "we estimate {est:,} reads in input file.\n".format(bytes=mean_bentry,
                                                                                      len=mean_len,
                                                                                      est=est_nlines))
     elif ext == '.sam':
-        with Reader(open(args.input.name)) as fh:
+        with Reader(open(file_name)) as fh:
             for read in fh:
                 sample_lengths.append(len(read))
                 sample_binsizes.append(len(str(read)))
@@ -81,40 +70,40 @@ def run(arguments):
             mean_bentry = mean(sample_binsizes)
             mean_len = mean(sample_lengths)
             est_nlines = int(bsize / mean_bentry)
-            if not args.quiet:
+            if not quiet:
                 sys.stderr.write("At {bytes:.0f} bytes per read of {len:.0f} length "
                                  "we estimate {est:,} reads in input file.\n".format(bytes=mean_bentry,
                                                                                      len=mean_len,
                                                                                      est=est_nlines))
     elif ext == '.bam':
-        est_nlines = sum(bam_read_count(args.input.name))
-        if not args.quiet:
+        est_nlines = sum(bam_read_count(file_name))
+        if not quiet:
             sys.stderr.write(
                 "{est:,} reads in input file.\n".format(est=est_nlines))
     elif ext == '.gz':
-        if args.binsize:
-            n = args.binsize
+        if binsize:
+            n = binsize
             est_nlines = None
-            if not args.quiet:
+            if not quiet:
                 sys.stderr.write(
                     "Reading from gzipped file, bin size (-s) set to {binsize:n}.\n".format(binsize=n))
         else:
             sys.stderr.write(
                 "Gzipped file detected. Reading file to determine bin size (-s).\n")
             p1 = Popen(shlex.split('gzip -dc %s' %
-                                   args.input.name), stdout=PIPE)
+                                   file_name), stdout=PIPE)
             p2 = Popen(shlex.split('wc -l'), stdin=p1.stdout, stdout=PIPE)
             est_nlines, _ = p2.communicate()
             est_nlines = int(est_nlines) // 4
-            if not args.quiet:
+            if not quiet:
                 sys.stderr.write(
                     "{est:,} reads in input file.\n".format(est=est_nlines))
     elif name == '<stdin>':
-        if args.binsize:
-            n = args.binsize
+        if binsize:
+            n = binsize
         else:
             n = 1
-        if not args.quiet:
+        if not quiet:
             sys.stderr.write(
                 "Reading from <stdin>, bin size (-s) set to {binsize:n}.\n".format(binsize=n))
         est_nlines = None
@@ -122,22 +111,22 @@ def run(arguments):
         sys.exit("The input file appears empty. Please check the file for data.")
     elif est_nlines is not None:
         # set up factor for sampling bin size
-        if args.binsize:
-            n = args.binsize
+        if binsize:
+            n = binsize
         else:
-            nf = math.floor(est_nlines / args.nreads)
+            nf = math.floor(est_nlines / nreads)
             if nf >= 1:
                 n = int(nf)
             else:
                 n = 1
-        if not args.quiet:
+        if not quiet:
             sys.stderr.write(
                 "Bin size (-s) set to {binsize:n}.\n".format(binsize=n))
 
     if ext in ['.sam', '.bam']:
-        infile = Reader(args.input)
+        infile = Reader(input)
     else:
-        infile = FastqReader(args.input, ext=ext)
+        infile = FastqReader(input, ext=ext)
 
     read_len = defaultdict(int)
     cycle_nuc = defaultdict(lambda: defaultdict(int))
@@ -149,7 +138,7 @@ def run(arguments):
                       'A': defaultdict(lambda: defaultdict(int)),
                       'T': defaultdict(lambda: defaultdict(int))}
 
-    if args.count_duplicates:
+    if count_duplicates:
         try:
             from pybloom import ScalableBloomFilter
             bloom_filter = ScalableBloomFilter(
@@ -163,6 +152,7 @@ def run(arguments):
 
     for read in reads:
         if isinstance(read, Sam):
+
             if args.aligned_only and not read.mapped:
                 continue
             elif args.unaligned_only and read.mapped:
@@ -177,20 +167,22 @@ def run(arguments):
             seq = read.seq
             qual = read.qual
 
+
         # Set up limits
-        if (args.leftlimit == 1) and (args.rightlimit < 0):
+        if (leftlimit == 1) and (rightlimit < 0):
             pass
-        elif (args.leftlimit >= 1) and (args.rightlimit > 0):
+        elif (leftlimit >= 1) and (rightlimit > 0):
             try:
-                seq = seq[args.leftlimit - 1:args.rightlimit]
-                qual = qual[args.leftlimit - 1:args.rightlimit]
+                seq = seq[leftlimit - 1:rightlimit]
+                qual = qual[leftlimit - 1:rightlimit]
             except IndexError:
                 act_nlines += n
                 continue
-        elif (args.leftlimit > 1) and (args.rightlimit < 0):
+
+        elif (leftlimit > 1) and (rightlimit < 0):
             try:
-                seq = seq[args.leftlimit - 1:]
-                qual = qual[args.leftlimit - 1:]
+                seq = seq[leftlimit - 1:]
+                qual = qual[leftlimit - 1:]
             except IndexError:
                 act_nlines += n
                 continue
@@ -199,19 +191,22 @@ def run(arguments):
             continue
         cycle_gc[gc(seq)] += 1
 
-        if args.count_duplicates:
+        if count_duplicates:
             if seq in bloom_filter:
                 duplicates += 1
             else:
                 bloom_filter.add(seq)
 
         for i, (s, q) in enumerate(zip(seq, qual)):
-            cycle_nuc[args.leftlimit + i][s] += 1
-            cycle_qual[args.leftlimit + i][q] += 1
+            cycle_nuc[leftlimit + i][s] += 1
+            cycle_qual[leftlimit + i][q] += 1
         read_len[len(qual)] += 1
 
-        for i, kmer in enumerate(window(seq, n=args.kmer)):
-            cycle_kmers[args.leftlimit + i][kmer] += 1
+        # ===========buggy above seq, qual==========
+
+
+        for i, k_mer in enumerate(window(seq, n=kmer)):
+            cycle_kmers[leftlimit + i][k_mer] += 1
 
         if isinstance(read, Sam) and read.mapped:
             try:
@@ -219,7 +214,7 @@ def run(arguments):
                 for i, (s, r) in enumerate(zip(seq, ref)):
                     if s != r:
                         try:
-                            cycle_mismatch[r][args.leftlimit + i][s] += 1
+                            cycle_mismatch[r][leftlimit + i][s] += 1
                         except KeyError:
                             pass
             except KeyError:
@@ -255,8 +250,8 @@ def run(arguments):
     # build kmer set of known adapter sequences
     adapter_kmers = set()
     for adapter in all_adapter_sequences:
-        for kmer in window(adapter, n=args.kmer):
-            adapter_kmers.add(kmer)
+        for k_mer in window(adapter, n=kmer):
+            adapter_kmers.add(k_mer)
 
     # test for nonuniform kmer profiles and calculate obs/exp
     observed_expected = dict()
@@ -264,17 +259,17 @@ def run(arguments):
     kmers = set(list(itertools.chain.from_iterable(all_kmers)))
     bad_kmers = []
     sequenced_bases = sum((l * n for l, n in read_len.items()))
-    priors = tuple(map(float, args.base_probs.split(',')))
-    for kmer in kmers:
-        kmer_counts = [(i, cycle_kmers[i][kmer])
+    priors = tuple(map(float, base_probs.split(',')))
+    for k_mer in kmers:
+        kmer_counts = [(i, cycle_kmers[i][k_mer])
                        for i in sorted(cycle_kmers.keys())]
         expected_fraction = reduce(
-            mul, (p ** kmer.count(b) for b, p in zip(('A', 'T', 'C', 'G', 'N'), priors)), 1)
+            mul, (p ** k_mer.count(b) for b, p in zip(('A', 'T', 'C', 'G', 'N'), priors)), 1)
         expected = expected_fraction * sequenced_bases
-        observed_expected[kmer] = sum((n for _, n in kmer_counts)) / expected
+        observed_expected[k_mer] = sum((n for _, n in kmer_counts)) / expected
         slope, _, _, p_value, _ = stats.linregress(*zip(*kmer_counts))
         if abs(slope) > 2 and p_value < 0.05:
-            bad_kmers.append((kmer, slope, p_value))
+            bad_kmers.append((k_mer, slope, p_value))
     bad_kmers = sorted(bad_kmers, key=lambda x: x[2])[:10]
 
     pos_gc = []
@@ -289,54 +284,53 @@ def run(arguments):
         pos_gc.append(pg)
 
     # see http://vita.had.co.nz/papers/tidy-data.pdf
-    args.text.write("{row}\t{column}\t{pos}\t{value:n}\n".format(
+    text.write("{row}\t{column}\t{pos}\t{value:n}\n".format(
         row=sample_name, column='reads', pos='None', value=act_nlines))
 
     for cycle, count in read_len.items():
-        args.text.write("{row}\t{column}\t{pos:n}\t{value:n}\n".format(row=sample_name, column='read_len', pos=cycle,
+        text.write("{row}\t{column}\t{pos:n}\t{value:n}\n".format(row=sample_name, column='read_len', pos=cycle,
                                                                        value=count))
 
     for i, position in enumerate(positions):
-        args.text.write("{row}\t{column}\t{pos:n}\t{value:n}\n".format(row=sample_name,
+        text.write("{row}\t{column}\t{pos:n}\t{value:n}\n".format(row=sample_name,
                                                                        column='q05', pos=position,
                                                                        value=quantiles[i][0]))
-        args.text.write("{row}\t{column}\t{pos:n}\t{value:n}\n".format(row=sample_name,
+        text.write("{row}\t{column}\t{pos:n}\t{value:n}\n".format(row=sample_name,
                                                                        column='q25', pos=position,
                                                                        value=quantiles[i][1]))
-        args.text.write("{row}\t{column}\t{pos:n}\t{value:n}\n".format(row=sample_name,
+        text.write("{row}\t{column}\t{pos:n}\t{value:n}\n".format(row=sample_name,
                                                                        column='q50', pos=position,
                                                                        value=quantiles[i][2]))
-        args.text.write("{row}\t{column}\t{pos:n}\t{value:n}\n".format(row=sample_name,
+        text.write("{row}\t{column}\t{pos:n}\t{value:n}\n".format(row=sample_name,
                                                                        column='q75', pos=position,
                                                                        value=quantiles[i][3]))
-        args.text.write("{row}\t{column}\t{pos:n}\t{value:n}\n".format(row=sample_name,
+        text.write("{row}\t{column}\t{pos:n}\t{value:n}\n".format(row=sample_name,
                                                                        column='q95', pos=position,
                                                                        value=quantiles[i][4]))
     for base in bases:
         for position in positions:
-            args.text.write("{row}\t{column}\t{pos:n}\t{value:n}\n".format(row=sample_name,
+            text.write("{row}\t{column}\t{pos:n}\t{value:n}\n".format(row=sample_name,
                                                                            column=base, pos=position,
                                                                            value=cycle_nuc[position][base]))
     for position in positions:
-        args.text.write("{row}\t{column}\t{pos:n}\t{value:n}\n".format(row=sample_name,
+        text.write("{row}\t{column}\t{pos:n}\t{value:n}\n".format(row=sample_name,
                                                                        column='pos_gc', pos=position,
                                                                        value=pos_gc[position - 1]))
     for i in range(101):
-        args.text.write("{row}\t{column}\t{pos:n}\t{value:n}\n".format(row=sample_name,
+        text.write("{row}\t{column}\t{pos:n}\t{value:n}\n".format(row=sample_name,
                                                                        column='read_gc', pos=i,
                                                                        value=cycle_gc[i]))
-
-    for kmer, obs_exp in sorted(observed_expected.items(), key=lambda x: x[1]):
-        args.text.write("{row}\t{column}\t{pos}\t{value:n}\n".format(row=sample_name,
-                                                                     column=kmer, pos='None',
+    for k_mer, obs_exp in sorted(observed_expected.items(), key=lambda x: x[1]):
+        text.write("{row}\t{column}\t{pos}\t{value:n}\n".format(row=sample_name,
+                                                                     column=k_mer, pos='None',
                                                                      value=obs_exp))
 
-    if args.count_duplicates:
-        args.text.write("{row}\t{column}\t{pos}\t{value:n}\n".format(
+    if count_duplicates:
+        text.write("{row}\t{column}\t{pos}\t{value:n}\n".format(
             row=sample_name, column='duplicate', pos='None', value=duplicates / act_nlines))
 
     from zipfile import ZipFile
-    with ZipFile(args.output + '.zip', mode='w') as zip_archive:
+    with ZipFile(output + '.zip', mode='w') as zip_archive:
         fig_kw = {'figsize': (8, 6)}
         qualplot(positions, quantiles, zip_archive, fig_kw)
         median_qual = qualdist(cycle_qual.values(), zip_archive, fig_kw)
@@ -352,21 +346,21 @@ def run(arguments):
             mismatchplot(positions, cycle_mismatch, zip_archive, fig_kw)
     time_finish = time.time()
     elapsed = time_finish - time_start
-    if not args.quiet:
+    if not quiet:
         sys.stderr.write("There were {counts:,} reads in the file. Analysis finished in {sec}.\n".format(counts=act_nlines,
                                                                                                          sec=time.strftime('%H:%M:%S',
                                                                                                                            time.gmtime(elapsed))
                                                                                                          ))
         if len(bad_kmers) > 0:
-            for kmer in bad_kmers:
+            for k_mer in bad_kmers:
                 sys.stderr.write(
-                    "KmerWarning: kmer %s has a non-uniform profile (slope = %s, p = %s).\n" % (kmer))
-        if median_qual < args.median_qual:
+                    "KmerWarning: kmer %s has a non-uniform profile (slope = %s, p = %s).\n" % (k_mer))
+        if median_qual < median_qual_r:
             sys.stderr.write(
                 "QualityWarning: median base quality score is %s.\n" % median_qual)
 
 
-def main(ext_args=None):
+def main(fname):
     parser = argparse.ArgumentParser(
         prog='fastqp', description="simple NGS read quality assessment using Python")
     parser.add_argument(
@@ -404,12 +398,13 @@ def main(ext_args=None):
     parser.add_argument('-d', '--count-duplicates', action="store_true", default=False,
                         help="calculate sequence duplication rate (default: %(default)s)")
 
-    if ext_args:
-        args = parser.parse_args(ext_args)
-    else:
-        args = parser.parse_args()
-    arguments = vars(args)
-    run(arguments)
+    # if ext_args:
+    #     args = parser.parse_args(ext_args)
+    # else:
+    #     args = parser.parse_args()
+    # arguments = vars(args)
+    run(fname)
 
 if __name__ == "__main__":
-    main()
+    fname = '/home/aneesh/coursera/fastq-jupyter/sra/ERR3653426.fastq'
+    main(fname)
